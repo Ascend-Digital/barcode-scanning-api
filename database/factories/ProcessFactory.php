c12efbe647b61aad2f7ebe90b9d2504a6844437f
TMAscend <tara.matthew@ascend.agency>
2024-05-10 16:38:53 +0100

    Add factories

---
+++ database/factories/ProcessFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Processes\Models\Process>
+ */
+class ProcessFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
