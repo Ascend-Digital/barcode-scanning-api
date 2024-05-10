e5a7bd55a959deba5b5ced5d561820aac428ada5
TMAscend <tara.matthew@ascend.agency>
2024-05-10 16:06:35 +0100

    Add database tables

---
+++ database/migrations/2024_05_10_134722_create_codes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Domain\Companies\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->foreignIdFor(Company::class);
+            $table->morphs('owner');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('codes');
+    }
+};
