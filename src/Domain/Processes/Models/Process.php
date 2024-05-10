bf99319720ff13d50c701c4df2be10bc17dc5b95
TMAscend <tara.matthew@ascend.agency>
2024-05-10 16:15:59 +0100

    Add models

---
+++ src/Domain/Processes/Models/Process.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain\Processes\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Process extends Model
+{
+    use HasFactory;
+}
