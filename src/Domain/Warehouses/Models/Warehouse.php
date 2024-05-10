297662d24bf8f9f64678ac8a2c48b24d225f91fc
TMAscend <tara.matthew@ascend.agency>
2024-05-10 16:15:59 +0100

    Add models

---
+++ src/Domain/Warehouses/Models/Warehouse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain\Warehouses\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Warehouse extends Model
+{
+    use HasFactory;
+}
---
+++ src/Domain/Workstations/Models/Workstation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain\Workstations\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Workstation extends Model
+{
+    use HasFactory;
+}
