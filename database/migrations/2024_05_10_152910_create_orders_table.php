4dcbbfba4fcf7e229f53aa2eae3811d74fcccaae
TMAscend <tara.matthew@ascend.agency>
2024-05-10 16:37:50 +0100

    Create orders table

---
+++ database/migrations/2024_05_10_152910_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Domain\Companies\Models\Company;
+use Domain\Statuses\Models\Status;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Status::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
