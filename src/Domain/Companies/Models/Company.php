331b563a584a70cc04a639512b1aec848aaa611d
TMAscend <tara.matthew@ascend.agency>
2024-05-13 11:42:29 +0100

    Add relationships

--- src/Domain/Companies/Models/Company.php
+++ src/Domain/Companies/Models/Company.php
@@ -2,14 +2,16 @@
 
 namespace Domain\Companies\Models;
 
+use Domain\Items\Models\Item;
 use Domain\Orders\Models\Order;
 use Domain\Processes\Models\Process;
+use Domain\Statuses\Models\Status;
 use Domain\Subscriptions\Models\Subscription;
+use Domain\Warehouses\Models\StorageLocation;
 use Domain\Warehouses\Models\Warehouse;
-use Domain\Workstations\Models\Workstation;
+use Domain\Warehouses\Models\Workstation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -37,8 +39,23 @@ class Company extends Model
         return $this->belongsToMany(Subscription::class);
     }
 
-    public function processes()
+    public function processes(): HasMany
     {
         return $this->hasMany(Process::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function statuses(): HasMany
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function storageLocations(): HasMany
+    {
+        return $this->hasMany(StorageLocation::class);
     }
 }
--- src/Domain/Warehouses/Models/StorageLocation.php
+++ src/Domain/Warehouses/Models/StorageLocation.php
@@ -2,10 +2,22 @@
 
 namespace Domain\Warehouses\Models;
 
+use Domain\Companies\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StorageLocation extends Model
 {
     use HasFactory;
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function warehouse(): BelongsTo
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
 }
