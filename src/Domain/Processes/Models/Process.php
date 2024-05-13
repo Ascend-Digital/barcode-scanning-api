f711a2c5c7ec1fce5222ffb061c304c103cc5a8d
TMAscend <tara.matthew@ascend.agency>
2024-05-13 08:35:23 +0100

    Continue setting up relationships

--- src/Domain/Processes/Models/Process.php
+++ src/Domain/Processes/Models/Process.php
@@ -2,10 +2,16 @@
 
 namespace Domain\Processes\Models;
 
+use Domain\Companies\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Process extends Model
 {
     use HasFactory;
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
--- src/Domain/Subscriptions/Models/Subscription.php
+++ src/Domain/Subscriptions/Models/Subscription.php
@@ -2,10 +2,16 @@
 
 namespace Domain\Subscriptions\Models;
 
+use Domain\Companies\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
     use HasFactory;
+
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class);
+    }
 }
