1d32804129e9af3ba0d3454cb11fadc85ef9fff1
TMAscend <tara.matthew@ascend.agency>
2024-05-13 11:39:57 +0100

    Add StorageLocation model

---
+++ src/Domain/Warehouses/Models/StorageLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Domain\Warehouses\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StorageLocation extends Model
+{
+    use HasFactory;
+}
