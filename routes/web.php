168039b36ff23306b81e13396ad785f4a76f3a79
TMAscend <tara.matthew@ascend.agency>
2024-05-13 15:17:29 +0100

    Remove route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,3 @@ Route::get('api/{version}/docs', function (string $version) {
         'url' => route('docs.definition', ['version' => $version]),
     ]);
 });
-
-Route::get('api/docs/{version}/definition', function (string $version) {
-    $filesystem = new Filesystem();
-
-    $path = base_path('/documentation/' . $version . '.json');
-    $contents = $filesystem->get($path);
-
-    return response($contents)->header('Content-Type', 'application/json');
-})->name('docs.definition');
