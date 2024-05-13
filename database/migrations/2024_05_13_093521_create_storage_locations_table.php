9e44432691747f539653bc1485fbf20157115eeb
TMAscend <tara.matthew@ascend.agency>
2024-05-13 11:02:13 +0100

    Add migration for storage_locations

---
+++ database/migrations/2024_05_13_093521_create_storage_locations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Domain\Companies\Models\Company;
+use Domain\Warehouses\Models\Warehouse;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('storage_locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Warehouse::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('storage_locations');
+    }
+};
