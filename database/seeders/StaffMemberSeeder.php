910d886027c45436ea32b4f5482b4cb8d7c593fa
TMAscend <tara.matthew@ascend.agency>
2024-05-14 12:37:35 +0100

    Add StaffMember factory and seeder

--- database/seeders/StaffMemberSeeder.php
+++ database/seeders/StaffMemberSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Domain\Users\Models\StaffMember;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class StaffMemberSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        StaffMember::factory(10)->create();
     }
 }
