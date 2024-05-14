efb45ece5e49a28fa6b736d4fcade297e2112b7b
TMAscend <tara.matthew@ascend.agency>
2024-05-14 13:35:48 +0100

    Add migration for item_storage_location

---
+++ database/migrations/2024_05_14_123243_create_item_storage_location_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Domain\Items\Models\Item;
+use Domain\Warehouses\Models\StorageLocation;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_storage_location', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Item::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(StorageLocation::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_storage_location');
+    }
+};
