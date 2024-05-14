a44147826a06ad353308ce02cec05079bc30e1c7
TMAscend <tara.matthew@ascend.agency>
2024-05-14 13:05:56 +0100

    Update migration

--- database/migrations/2024_05_10_134836_create_processes_table.php
+++ database/migrations/2024_05_10_134836_create_processes_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use Domain\Companies\Models\Company;
+use Domain\Statuses\Models\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +16,7 @@ return new class extends Migration
         Schema::create('processes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignIdFor(Company::class)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId('from_status')->constrained(table: 'statuses')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('to_status')->constrained(table: 'statuses')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
