db58657ed83aca43a11d9fb49f14bd2a49c9d3ba
TMAscend <tara.matthew@ascend.agency>
2024-06-05 16:13:59 +0100

    [MZUR-223] Add method

--- tests/Unit/Policies/OrderPolicyTest.php
+++ tests/Unit/Policies/OrderPolicyTest.php
@@ -12,10 +12,12 @@ it('is set up correctly for admin and non-admin users', function (User $user, st
     Gate::policy(Order::class, OrderPolicy::class);
     $this->assertEquals($expectedResult, Gate::forUser($user)->allows($action, $order));
 })->with([
+    [fn () => User::factory()->create(['email' => 'test@ascend.agency']), 'action' => 'view-any', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'test@ascend.agency']), 'action' => 'view', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'test2@ascend.agency']), 'action' => 'create', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'test3@ascend.agency']), 'action' => 'update', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'dev@ascend.agency']), 'action' => 'delete', 'expectedResult' => true],
+    [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'view-any', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'view', 'expectedResult' => true],
     [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'create', 'expectedResult' => false],
     [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'update', 'expectedResult' => false],
