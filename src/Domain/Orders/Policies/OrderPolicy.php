038b1236888a0d5a239487b84f0fdfd7fe5d2a87
TMAscend <tara.matthew@ascend.agency>
2024-06-05 16:12:48 +0100

    [MZUR-223] Update test and policy

--- src/Domain/Orders/Policies/OrderPolicy.php
+++ src/Domain/Orders/Policies/OrderPolicy.php
@@ -4,4 +4,28 @@ namespace Domain\Orders\Policies;
 
 class OrderPolicy
 {
+    public function viewAny(): bool
+    {
+        return true;
+    }
+
+    public function view(): bool
+    {
+        return true;
+    }
+
+    public function create(): bool
+    {
+        return false;
+    }
+
+    public function update(): bool
+    {
+        return false;
+    }
+
+    public function delete(): bool
+    {
+        return false;
+    }
 }
