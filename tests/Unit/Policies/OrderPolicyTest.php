c21e53e6b9dc4b99788fbc57817aa35c2631827b
TMAscend <tara.matthew@ascend.agency>
2024-06-05 15:43:12 +0100

    [MZUR-223] Add tests

---
+++ tests/Unit/Policies/OrderPolicyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('allows a user to perform all policy actions', function (User $user, string $action, bool $expectedResult) {
+    $this->assertEquals($expectedResult, Gate::forUser($user)->allows($action));
+})->with([
+    [fn () => User::factory()->create(['email' => 'test@ascend.agency']), 'action' => 'view-order', 'expectedResult' => true],
+    [fn () => User::factory()->create(['email' => 'test2@ascend.agency']), 'action' => 'create-order', 'expectedResult' => true],
+    [fn () => User::factory()->create(['email' => 'test3@ascend.agency']), 'action' => 'update-order', 'expectedResult' => true],
+    [fn () => User::factory()->create(['email' => 'dev@ascend.agency']), 'action' => 'delete-order', 'expectedResult' => true],
+    [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'view-order', 'expectedResult' => false],
+    [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'create-order', 'expectedResult' => false],
+    [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'update-order', 'expectedResult' => false],
+    [fn () => User::factory()->create(['email' => 'not-ascend-email@example.com']), 'action' => 'delete-order', 'expectedResult' => false],
+]);
