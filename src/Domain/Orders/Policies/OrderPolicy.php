95bcc06dc0c18a31e79066621961c661f89710b2
TMAscend <tara.matthew@ascend.agency>
2024-06-05 15:14:03 +0100

    [MZUR-223] Empty out policy

--- src/Domain/Orders/Policies/OrderPolicy.php
+++ src/Domain/Orders/Policies/OrderPolicy.php
@@ -2,43 +2,6 @@
 
 namespace Domain\Orders\Policies;
 
-use Domain\Orders\Models\Order;
-use Domain\Users\Models\User;
-
 class OrderPolicy
 {
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    public function view(User $user, Order $order): bool
-    {
-        //
-    }
-
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    public function update(User $user, Order $order): bool
-    {
-        //
-    }
-
-    public function delete(User $user, Order $order): bool
-    {
-        //
-    }
-
-    public function restore(User $user, Order $order): bool
-    {
-        //
-    }
-
-    public function forceDelete(User $user, Order $order): bool
-    {
-        //
-    }
 }
