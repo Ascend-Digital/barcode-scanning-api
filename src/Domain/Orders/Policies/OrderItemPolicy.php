584308295f53966cda53b934a9d53e44fab546dc
TMAscend <tara.matthew@ascend.agency>
2024-06-05 15:23:52 +0100

    [MZUR-221] Update policies

--- src/Domain/Orders/Policies/OrderItemPolicy.php
+++ src/Domain/Orders/Policies/OrderItemPolicy.php
@@ -7,38 +7,8 @@ use Domain\Users\Models\User;
 
 class OrderItemPolicy
 {
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
     public function view(User $user, OrderItem $orderItem): bool
     {
-        //
-    }
-
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    public function update(User $user, OrderItem $orderItem): bool
-    {
-        //
-    }
-
-    public function delete(User $user, OrderItem $orderItem): bool
-    {
-        //
-    }
-
-    public function restore(User $user, OrderItem $orderItem): bool
-    {
-        //
-    }
-
-    public function forceDelete(User $user, OrderItem $orderItem): bool
-    {
-        //
+        return true;
     }
 }
