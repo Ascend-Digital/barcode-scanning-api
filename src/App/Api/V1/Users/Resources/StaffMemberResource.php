339accbdc3a51c71fcb1c5e6149f9f994a64c018
TMAscend <tara.matthew@ascend.agency>
2024-06-10 16:39:41 +0100

    [MZUR-201] Update

--- src/App/Api/V1/Users/Resources/StaffMemberResource.php
+++ src/App/Api/V1/Users/Resources/StaffMemberResource.php
@@ -3,11 +3,10 @@
 namespace App\Api\V1\Users\Resources;
 
 use App\Api\V1\Companies\Resources\CompanyResource;
-use Domain\Barcodes\Contracts\ScannableResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class StaffMemberResource extends JsonResource implements ScannableResource
+class StaffMemberResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
@@ -17,18 +16,6 @@ class StaffMemberResource extends JsonResource implements ScannableResource
             'name' => $this->name,
             'company' => new CompanyResource($this->whenLoaded('company')),
             'actions' => $this->availableActions(),
-        ];
-    }
-
-    public function availableActions(): array
-    {
-        return [
-            /*
-            [
-                'name' => 'Pick from storage location',
-                'endpoint' => route('storage-locations.item.pick')
-            ],
-            */
         ];
     }
 }
