fab6728bead776af77e8531181192653aa9b9603
TMAscend <tara.matthew@ascend.agency>
2024-06-10 16:44:06 +0100

    [MZUR-201] Update resource

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/barcodes/{barcode:barcode}', ScanBarcodeController::class)->name('barcodes.scan')->middleware(LogScan::class);
+Route::get('/barcodes/{barcode:barcode}', ScanBarcodeController::class)->name('barcodes.scan');
--- src/App/Api/V1/Companies/Resources/CompanyResource.php
+++ src/App/Api/V1/Companies/Resources/CompanyResource.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CompanyResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray($request): array
     {
         return [
