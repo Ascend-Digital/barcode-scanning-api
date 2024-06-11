734cdfa561a11e1c127141f2c4dc11ebe795c6b8
TMAscend <tara.matthew@ascend.agency>
2024-06-11 10:57:01 +0100

    [MZUR-201] Remove route

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Api\V1\Barcodes\Controllers\ScanBarcodeController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::get('/barcodes/{barcode:barcode}', ScanBarcodeController::class)->name('barcodes.scan');
